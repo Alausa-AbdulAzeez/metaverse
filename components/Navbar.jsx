91096227de529d4a6aacbb9c93f036c874eca2be
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-09-03 05:43:38 +0100

    project setup complete

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import styles from "@/styles";
+import { newVariants } from "@/utils/motion";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
---
+++ components/StartSteps.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StartSteps = () => {
+  return <div>StartSteps</div>;
+};
+
+export default StartSteps;
