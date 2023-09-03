51acff2a6b72b4db14b7e385c7bfb8cc446576fa
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-09-03 09:53:09 +0100

    completed hero

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import styles from "@/styles";
-import { newVariants, navVariants } from "@/utils/motion";
+import { navVariants } from "@/utils/motion";
 
 const Navbar = () => {
   return (
