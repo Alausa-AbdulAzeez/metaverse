2c298bd9c59e5a31ef0f6d38ad6fb62756f20692
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-09-04 13:31:09 +0100

    completed the explore section

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,35 +1,35 @@
-"use client";
-import React, { useState } from "react";
-import { motion } from "framer-motion";
-import { exploreWorlds } from "../constants";
-import { TitleText, TypingText, ExploreCard } from "../components";
-import styles from "@/styles";
-import { staggerContainer } from "@/utils/motion";
+'use client'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
+import { exploreWorlds } from '../constants'
+import { TitleText, TypingText, ExploreCard } from '../components'
+import styles from '@/styles'
+import { staggerContainer } from '@/utils/motion'
 
 const Explore = () => {
-  const [active, setActive] = useState("world-2");
+  const [active, setActive] = useState('world-2')
 
   return (
-    <section id="explore" className={`${styles.paddings}`}>
+    <section id='explore' className={`${styles.paddings}`}>
       <motion.div
         variants={staggerContainer}
-        initial="hidden"
-        whileInView="show"
+        initial='hidden'
+        whileInView='show'
         viewport={{ once: false, amount: 0.25 }}
-        className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
       >
-        <TypingText title="| The World" textStyles="text-center" />
+        <TypingText title='| The World' textStyles='text-center' />
         <TitleText
           title={
             <>
-              {" "}
-              Choose the world you want <br className="md:block hidden" /> to
+              {' '}
+              Choose the world you want <br className='md:block hidden' /> to
               explore
             </>
           }
-          textStyles="text-center"
+          textStyles='text-center'
         />
-        <div className="flex lg:flex-row flex-col min-h-[70vh] gap-5 mt-[50px]">
+        <div className='flex lg:flex-row flex-col min-h-[70vh] gap-5 mt-[50px]'>
           {exploreWorlds?.map((world, index) => {
             return (
               <ExploreCard
@@ -39,12 +39,12 @@ const Explore = () => {
                 active={active}
                 handleClick={setActive}
               />
-            );
+            )
           })}
         </div>
       </motion.div>
     </section>
-  );
-};
+  )
+}
 
-export default Explore;
+export default Explore
